8b964bfad02a714bf2b8921d759257003d9b8c91
Alvin T <Alvin T>
2017-03-13 11:30:55 +0700

    initial commit

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootsrtap.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+				<div class="col-lg-12">
+					<h1>ToDo List</h1>
+					<ul>
+						<li>Taki</li>
+						<li>Mitsuha</li>
+						<li>Yotsuha</li>
+					</ul>
+					<form>
+						<div class="row">
+								<div class="col-lg-3">
+									<div class="form-group">
+										<input type="text" class="form-control" name="todo_item" placeholder="Item baru...">
+									</div>
+								</div>
+								<div class="col-lg-1">
+									<button type="submit" class="btn btn-success">Add</button>
+								</div>
+								<div class= "col-lg-8">	</div>
+						</div>
+					</form>
+					<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootsrtap.min.js"></script>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
