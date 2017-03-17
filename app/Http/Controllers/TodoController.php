5613cca6c6e9d4041496b77e0edb510235c6fd27
Alvin <alvin.valor@gmail.com>
2017-03-17 23:05:34 +0700

    Add files via upload

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -7,19 +7,27 @@ use App\Todo;
 
 class TodoController extends Controller
 {
-    public function index(){
-    	$todo=Todo::all();
+    public function index()
+    {
+    	$todo = Todo::all();
     	return view('todo')->with('todo', $todo);
     }
 
     public function store(Request $request)
-    {
-    	$todo_item=$request->input('todo_item');
-    	$todo = new Todo;
-    	$todo->item=$todo_item;
-    	$todo->save();
+	{
+		$todo_item = $request->input('todo_item');
+		$todo = new Todo;
+		$todo->item = $todo_item;
+		$todo->save();
 
-    	$todo=Todo::all();
-    	return view('todo')->with('todo', $todo);
+		return redirect('todo');
+	}
+
+	 public function destroy($id_todo) 
+    {
+    	$todo= Todo::find($id_todo);
+        $todo->delete();
+        
+        return redirect('todo');
     }
 }
