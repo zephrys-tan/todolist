6de772e8c2c464866daef3c7eb07efff560f4538
Alvin <alvin.valor@gmail.com>
2017-03-17 23:06:19 +0700

    Add files via upload

--- app/Todo.php
+++ app/Todo.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    protected $primarykey='id_todo';
-    protected $fillable=['item'];
+    protected $primaryKey = 'id_todo';
+
+    protected $fillable = [
+    	'item'
+    ];
 }
